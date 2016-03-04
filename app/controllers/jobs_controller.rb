54d7e8c7d09bad180ab1306456cf9b0d46500715
jkopp <jkopp@mkei.org>
2016-03-04 12:20:45 -0600

    Before_action bug fix in job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user,   only: [:create, :destroy]
+  before_action :correct_user,   only: [:destroy]
 
   def index
     if Job.any?
